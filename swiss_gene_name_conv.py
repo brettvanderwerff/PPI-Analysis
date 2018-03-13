1592be77cfaf835875013ea6cf51c844869358bb
Brett.Vanderwerff <vande060@gmail.com>
2018-03-13 16:24:02 -0700

    completed this script, now has some functionality

--- swiss_gene_name_conv.py
+++ swiss_gene_name_conv.py
@@ -1,7 +1,12 @@
+'''
+Script locates common gene names for swissprot IDs in a dataframe returned from the id_converter script. The dictionary
+referenced is 'HUMAN_9606_idmapping.dat' downloaded from:
+ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/
+'''
 import id_converter
 import id_parser
+import numpy as np
 import pandas as pd
-
 
 def find_gene_name(id_converted_df, label):
     '''Finds the corresponding common gene name for each human swissprot ID in the df returned by the run function
@@ -9,10 +14,24 @@ def find_gene_name(id_converted_df, label):
     '''
     gene_name_reader = pd.read_csv('HUMAN_9606_idmapping.dat', delimiter="\t", header=None)
     gene_name_only_df = gene_name_reader.loc[gene_name_reader[1] == 'Gene_Name']
+    gene_name_list = []
     for item in id_converted_df[label]:
-        print(gene_name_only_df.loc[gene_name_only_df[0] == item])
+        try:
+            gene_name_list.append(gene_name_only_df.loc[gene_name_only_df[0] == item][2].iloc[0])
+        except:
+            gene_name_list.append(np.nan)
+    return pd.DataFrame(gene_name_list, columns=[label + ' gene name'])
+
+def run(id_converted_df):
+    '''Calls the find_gene_name function once to convert protein A swissprot ID to common gene names and again to
+     convert protein B swissprot ID to common gene names and returns a concatenated dataframe with the newly added
+    gene names.
+    '''
+    gene_names_A = find_gene_name(id_converted_df=id_converted_df, label='Parsed A ID')
+    gene_names_B = find_gene_name(id_converted_df=id_converted_df, label='Parsed B ID')
+    return pd.concat([id_converted_df, gene_names_A, gene_names_B], axis=1)
 
 if __name__ == '__main__':
     id_parsed_df = id_parser.run(filename='clusteredQuery_MST1R.txt')
     id_converted_df = id_converter.run(id_parsed_df=id_parsed_df)
-    find_gene_name(id_converted_df=id_converted_df, label='Parsed A ID')
+    run(id_converted_df=id_converted_df)
