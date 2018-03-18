cb725e66e76f53fd0e58720a88dff7304372a82a
Brett.Vanderwerff <vande060@gmail.com>
2018-03-18 12:37:59 -0700

    added function to check if biogrid swiss conversion file is already written to the directory

--- biogrid_swiss_conv.py
+++ biogrid_swiss_conv.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import os.path
+
+def check_install():
+    ''' Checks if 'biogrid_swiss_conversion.csv' is installed to the directory, returns True if it is.
+    '''
+    print('checking if biogrid_swiss_conversion.csv is installed.')
+    return True if os.path.isfile('biogrid_swiss_conversion.csv') else False
+
 
 def get_biogrid_swiss_id_conv(in_filename, out_filename):
     '''Processes Biogrid ID mapping file 'BIOGRID-IDENTIFIERS-3.4.158.tab.txt' gotten from:
@@ -12,6 +20,15 @@ def get_biogrid_swiss_id_conv(in_filename, out_filename):
     biogrid_swissprot_ids = read_file_trimmed[['BIOGRID_ID' , 'IDENTIFIER_VALUE']]
     return biogrid_swissprot_ids.to_csv(out_filename, sep='\t', index=False)
 
+def run():
+    '''Calls all functions in script in order.
+    '''
+    if check_install() == True:
+        print('Biogrid swissprot conversion file is already installed')
+    else:
+        print('Installing Biogrid swissprot conversion file, this will take several minutes...')
+        get_biogrid_swiss_id_conv(in_filename='BIOGRID-IDENTIFIERS-3.4.158.tab.txt',
+                                  out_filename='biogrid_swiss_conversion.csv')
+
 if __name__ ==  '__main__':
-    get_biogrid_swiss_id_conv(in_filename='BIOGRID-IDENTIFIERS-3.4.158.tab.txt',
-                              out_filename='biogrid_swiss_conversion.csv')
+    run()
